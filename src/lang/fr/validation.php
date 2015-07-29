4813659cbdee0976e18481a6e55294ea8aaf44b8
MyoStudio <a.zuber@myostudio.com>
2015-07-29 11:15:12 +0200

    Fix french errors

--- src/lang/fr/validation.php
+++ src/lang/fr/validation.php
@@ -1,15 +1,13 @@
 <?php
 
-return array(
-
+return [
     'iban' => 'Le champ :attribute doit être un code banque international valide (IBAN).',
-    'bic' => 'Le champs :attribute n\'est pas in un code valide.',
-    'hexcolor' => 'e champ :attribute doit être un code couleur hexadécimal valide.',
+    'bic' => 'Le champ :attribute n\'est pas un un code valide.',
+    'hexcolor' => 'Le champ :attribute doit être un code couleur hexadécimal valide.',
     'creditcard' => 'Le champ :attribute doit être un numéro de carte de crédit valide.',
     'isbn' => 'L\':attribute doit être un ISBN valide.',
     'isodate' => 'La valeur de :attribute doit être une date au format ISO 8601.',
-    'username' => 'La valeur :attribute doit être un pseudo valide.',
-    'htmlclean' => 'La valeur :attribute contient du code HTML interdit.',
-    'password' => ':attribute doit contenir entre 6 et 64 caractères y compris au moins un chiffre, une lettre majuscule, une lettre minuscule et un caractère spécial.'
-
-);
+    'username' => 'La valeur de :attribute doit être un pseudo valide.',
+    'htmlclean' => 'La valeur de :attribute contient du code HTML interdit.',
+    'password' => 'Le :attribute doit contenir entre 6 et 64 caractères y compris au moins un chiffre, une lettre majuscule, une lettre minuscule et un caractère spécial.'
+];
