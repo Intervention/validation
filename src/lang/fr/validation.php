dcd737e626c67b73368c7e2cc994ed42222029ec
Rémy MyoStudio <r.baudon@myostudio.com>
2015-07-29 10:31:33 +0200

    Set french trad for validation messages

---
+++ src/lang/fr/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+    'iban' => 'Le :attribute doit être un code banque international valide (IBAN).',
+    'bic' => 'Le :attribute n\'est pas in un code valide.',
+    'hexcolor' => 'Le :attribute doit être un code couleur hexadécimal valide.',
+    'creditcard' => 'Le :attribute doit être un numéro de carte de crédit valide.',
+    'isbn' => ':attribute doit être un ISBN valide.',
+    'isodate' => 'La valeur de :attribute doit être une date au format ISO 8601.',
+    'username' => 'La valeur :attribute doit être un pseudo valide.',
+    'htmlclean' => 'La valeur :attribute contient du code HTML interdit.',
+    'password' => ':attribute doit contenir entre 6 et 64 caractères y compris au moins un chiffre, une lettre majuscule, une lettre minuscule et un caractère spécial.'
+
+);
