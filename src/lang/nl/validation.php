3e806f62e2daae502411a00f9fb1fd89d359c76e
jan.lindenholz@gmail.com <janlindenholz@gmail.com>
2017-09-22 12:15:41 +0200

    dutch validation

---
+++ src/lang/nl/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+    'isin' => ':attribute moet een geldig International Securities Identification Number (ISIN) zijn.',
+    'iban' => ':attribute moet een geldig International Bank Account Number (IBAN) zijn.',
+    'bic' => ':attribute is geen geldige Business Identifier Code (BIC) zijn.',
+    'hexcolor' => ':attribute moet een geldige hexadecimal kleurcode zijn.',
+    'creditcard' => ':attribute moet een geldig creditkaart nummer zijn.',
+    'isbn' => ':attribute moet een geldig International Standard Book Number (ISBN) zijn.',
+    'isodate' => ':attribute moet een geldig datum in ISO 8601 formaat zijn.',
+    'username' => ':attribute moet een geldige gebruikersnaam zijn.',
+    'htmlclean' => ':attribute bevat verboden HTML code.',
+    'password' => ':attribute moet tussen de 6 en 64 karakers zijn en tenminste één nummer, één hoofdletter, één gewone letter en één speciaal teken bevatten.',
+    'alpha_space' => ':attribute mag alleen uit letters en spaties bestaan.',
+    'domainname' => ':attribute moet een geldige domeinnaam zijn.',
+    'empty_with' => ':attribute of de andere velden moeten ingevuld zijn, niet alle velden.',
+
+);
