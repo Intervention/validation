4f2d07d8f0e02574de4212e9dcd6c435f654f94c
Oliver Vogel <oliver@olivervogel.com>
2020-02-15 17:49:13 +0100

    Refactored ISBN validation rule

--- src/Rules/Isbn.php
+++ src/Rules/Isbn.php
@@ -13,43 +13,78 @@ class Isbn extends AbstractStringRule
      */
     public function isValid()
     {
-        $value = str_replace(array(' ', '-', '.'), '', $this->getValue());
-        $length = strlen($value);
-        $checkdigit = substr($value, -1);
-
-        if ($length == 10) {
-            if (! is_numeric(substr($value, -10, 9))) {
-                return false;
-            }
-
-            $checkdigit = ( ! is_numeric($checkdigit)) ? $checkdigit : strtoupper($checkdigit);
-            $checkdigit = ($checkdigit == 'X') ? '10' : $checkdigit;
-
-            $sum = 0;
-
-            for ($i = 0; $i < 9; $i++) {
-                $sum = $sum + ($value[$i] * (10 - $i));
-            }
-
-            $sum = $sum + $checkdigit;
-            $mod = $sum % 11;
-
-            return ($mod == 0);
-        } elseif ($length == 13) {
-            $sum = 0;
-
-            $sum =  $value[0] + ($value[1] * 3) + $value[2] + ($value[3] * 3) +
-                    $value[4] + ($value[5] * 3) + $value[6] + ($value[7] * 3) +
-                    $value[8] + ($value[9] * 3) + $value[10] + ($value[11] * 3);
-
-            $mod = $sum % 10;
-
-            $correct_checkdigit = 10 - $mod;
-            $correct_checkdigit = ($correct_checkdigit == "10") ? "0" : $correct_checkdigit;
-
-            return ($checkdigit == $correct_checkdigit);
+        switch (strlen($this->getValue())) {
+            case 10:
+                return $this->shortChecksumMatches();
+            
+            case 13:
+                return $this->longChecksumMatches();
         }
 
         return false;
+    }
+
+    /**
+     * Determine if checksum for short ISBN numbers is valid
+     *
+     * @return bool
+     */
+    private function shortChecksumMatches()
+    {
+        return $this->getShortChecksum() % 11 === 0;
+    }
+
+    /**
+     * Calculate checksum of short ISBN numbers
+     *
+     * @return int
+     */
+    private function getShortChecksum()
+    {
+        $checksum = 0;
+        $multiplier = 10;
+        foreach (str_split($this->getValue()) as $digit) {
+            $digit = strtolower($digit) == 'x' ? 10 : intval($digit);
+            $checksum += $digit * $multiplier;
+            $multiplier--;
+        }
+
+        return $checksum;
+    }
+
+    /**
+     * Determine if long checksum is valid
+     *
+     * @return bool
+     */
+    private function longChecksumMatches()
+    {
+        return $this->getLongChecksum() % 10 === 0;
+    }
+
+    /**
+     * Calculate checksum for long ISBN numbers
+     *
+     * @return int
+     */
+    private function getLongChecksum()
+    {
+        $checksum = 0;
+        foreach (str_split($this->getValue()) as $num => $digit) {
+            $multiplier = $num % 2 ? 3 : 1;
+            $checksum += intval($digit) * $multiplier;
+        }
+
+        return $checksum;
+    }
+
+    /**
+     * Prepare value to validate
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return preg_replace("/[^0-9x]/i", '', parent::getValue());
     }
 }
