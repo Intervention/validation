b129b2b804e7014655d26ffe998676dd7fab350d
Oliver Vogel <oliver@olivervogel.com>
2020-03-20 17:40:45 +0100

    Rewrite Domainname rule

--- src/Rules/Domainname.php
+++ src/Rules/Domainname.php
@@ -2,12 +2,10 @@
 
 namespace Intervention\Validation\Rules;
 
-use Intervention\Validation\AbstractRegexRule;
+use Intervention\Validation\AbstractStringRule;
 
-class Domainname extends AbstractRegexRule
+class Domainname extends AbstractStringRule
 {
-    protected $pattern = "/^(?!\-)(?:[a-zA-Z\d\-]{0,62}[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{1,63}$/";
-
     /**
      * Determine if current input is valid
      *
@@ -15,10 +13,86 @@ class Domainname extends AbstractRegexRule
      */
     public function isValid()
     {
-        if (strlen($this->getValue()) > 253) {
+        $labels = $this->getLabels(); // get labels
+        $tld = end($labels); // most right label is tld
+
+        // min. 2 labels
+        if (count($labels) <= 1) {
             return false;
         }
 
-        return parent::isValid();
+        // validate each label
+        foreach ($labels as $label) {
+            if (! $this->isValidLabel($label)) {
+                return false;
+            }
+        }
+
+        // validate tld
+        if (! $this->isValidTld($tld)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get all labels of domainname
+     *
+     * @return array
+     */
+    private function getLabels(): array
+    {
+        return explode('.', idn_to_ascii(parent::getValue()));
+    }
+
+    /**
+     * Determine if given string is valid idn label
+     *
+     * @param  string  $value
+     * @return boolean
+     */
+    private function isValidLabel(string $value): bool
+    {
+        return $this->isValidALabel($value) || $this->isValidNrLdhLabel($value);
+    }
+
+    /**
+     * Determine if given value is valid A-Label
+     *
+     * Begins with "xn--" and is resolvable by PunyCode algorithm
+     *
+     * @param  string  $value
+     * @return boolean
+     */
+    private function isValidALabel(string $value): bool
+    {
+        return substr($value, 0, 4) === 'xn--' && idn_to_utf8($value) !== false;
+    }
+
+    /**
+     * Determine if given value is valud NR-LDH label
+     *
+     * @param  string  $string
+     * @return boolean
+     */
+    private function isValidNrLdhLabel(string $value): bool
+    {
+        return (bool) preg_match("/^(?!\-)[a-z0-9\-]{1,63}(?<!\-)$/i", $value);
+    }
+
+    /**
+     * Determine if given value is valid TLD
+     *
+     * @param  string  $value
+     * @return boolean
+     */
+    private function isValidTld(string $value): bool
+    {
+        if ($this->isValidALabel($value)) {
+            return true;
+        }
+
+        return (bool) preg_match("/^[a-z]{2,63}$/i", $value);
     }
 }
