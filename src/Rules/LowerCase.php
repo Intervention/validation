5342b0f8a45b2f14089b41c4abe7baf2164d57fa
Oliver Vogel <oliver@olivervogel.com>
2020-03-22 21:25:19 +0100

    Fix

--- src/Rules/LowerCase.php
+++ src/Rules/LowerCase.php
@@ -4,7 +4,7 @@ namespace Intervention\Validation\Rules;
 
 use Intervention\Validation\AbstractRegexRule;
 
-class Lowercase extends AbstractRegexRule
+class LowerCase extends AbstractRegexRule
 {
     /**
      * Regular expression pattern for lower case string
