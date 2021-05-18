a5c82d9896eb56379eeac9d666bb5c3f2ea76246
Oliver Vogel <oliver@olivervogel.com>
2021-05-18 16:19:05 +0200

    Updated IBAN lengths

--- tests/Rules/IbanTest.php
+++ tests/Rules/IbanTest.php
@@ -12,6 +12,10 @@ class IbanTest extends AbstractRuleTestCase
     protected $valid = [
         'DE12500105170648489890',
         'GB82 WEST 1234 5698 7654 32',
+        'PK36SCBL0000001123456702',
+        'QA 54QNBA0000 00000000 693123456',
+        'CI93CI0080111301134291200589',
+        'NI92BAMC000000000000000003123123',
     ];
 
     /**
@@ -23,5 +27,7 @@ class IbanTest extends AbstractRuleTestCase
         'DE21340155170648089890',
         'GR82 WEST 1234 5698 7654 32',
         '5070081',
+        'KM4600005010010904400137',
+        'SA4420000001234567891231',
     ];
 }
