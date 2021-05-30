ae48c1301ffde798ef178d533ffa80bb69fb4267
bogdan <bogdan.nesterenko@gmail.com>
2021-05-30 17:32:21 +0300

    Take into account base64 encoded image
    Faced with the issue that doesn't allow to validate base64 encoded image. 
    Here is a short fix for that

--- src/Rules/Base64.php
+++ src/Rules/Base64.php
@@ -15,4 +15,17 @@ class Base64 extends AbstractStringRule
     {
         return base64_encode(base64_decode($this->getValue(), true)) === $this->getValue();
     }
+    
+    /**
+     * Take into account base64 encoded image and return the current value
+     *
+     * @return mixed
+     */
+    public function getValue() {
+        $value = parent::getValue();
+        $pattern = "/^data:(?:image\/[a-zA-Z\-\.]+)(?:\+xml)?(?:charset=\".+\")?;base64,(?P<data>.+)$/";
+        preg_match($pattern, $value, $matches);
+
+        return isset($matches['data']) ? data_get($matches, 'data') : $value;
+    }
 }
