4cd59b0113dcdf9a338a02f6df6160e0298c1c98
Oliver Vogel <oliver@olivervogel.com>
2021-06-01 17:57:28 +0200

    Refactoring

--- src/Rules/Ean.php
+++ src/Rules/Ean.php
@@ -28,12 +28,23 @@ class Ean extends AbstractStringRule
      */
     protected function checksumMatches(): bool
     {
+        return $this->getModuloChecksum($this->getValue()) === intval(substr($this->getValue(), -1));
+    }
+
+    /**
+     * Calculate modulo checksum of given value
+     *
+     * @param  mixed $value
+     * @return int
+     */
+    protected function getModuloChecksum($value): int
+    {
         $checksum = 0;
-        $chars = array_reverse(str_split(substr($this->getValue(), 0, -1), 1));
+        $chars = array_reverse(str_split(substr($value, 0, -1), 1));
         foreach ($chars as $key => $char) {
             $checksum += ($key % 2 === 1) ? intval($char) * 1 : intval($char) * 3;
         }
 
-        return 10 - $checksum % 10 === intval(substr($this->getValue(), -1));
+        return 10 - $checksum % 10;
     }
 }
