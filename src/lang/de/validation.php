3a321e38631e230079e498cb40f8d9cbb774280c
Oliver Vogel <oliver@olivervogel.com>
2022-02-12 17:08:34 +0100

    Corrected german spelling

--- src/lang/de/validation.php
+++ src/lang/de/validation.php
@@ -4,7 +4,7 @@ return [
     'isin' => 'Der Wert :attribute ist keine International Securities Identification Number (ISIN).',
     'iban' => 'Der Wert :attribute ist keine International Bank Account Number (IBAN).',
     'bic' => 'Der Wert :attribute muss einen gültigen Business Identifier Code (BIC) enthalten.',
-    'hexcolor' => 'Der Wert :attribute muss einen gültigen Hexadezimal Farbwert enthalten.',
+    'hexcolor' => 'Der Wert :attribute muss einen gültigen hexadezimalen Farbwert enthalten.',
     'creditcard' => 'Der Wert :attribute ist keine gültige Kreditkartennummer.',
     'isbn' => 'Der Wert :attribute muss eine gültige International Standard Book Number (ISBN) enthalten.',
     'username' => 'Der Wert :attribute enthält keinen gültigen Benutzernamen.',
