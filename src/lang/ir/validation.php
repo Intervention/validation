a9f5e79019ef71a867203427bc68a48e00ff5718
arya <arya.lavasani@gmail.com>
2024-03-26 16:36:40 +0330

    [feat] add persian language

---
+++ src/lang/ir/validation.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'isin' => 'مقدار :attribute باید یک کد ISIN معتبر باشد.',
+    'iban' => 'مقدار :attribute باید یک شماره شبا معتبر باشد.',
+    'bic' => 'مقدار :attribute باید یک کد BIC معتبر باشد.',
+    'hexadecimalcolor' => 'مقدار :attribute باید یک کد رنگ HEX معتبر باشد.',
+    'creditcard' => 'مقدار :attribute باید یک شماره کارت اعتباری معتبر باشد.',
+    'isbn' => 'مقدار :attribute باید یک شماره ISBN معتبر باشد.',
+    'username' =>  'مقدار :attribute باید یک نام کاربری معتبر باشد.',
+    'htmlclean' => 'مقدار :attribute باید یک متن HTML معتبر باشد.',
+    'domainname' => 'مقدار :attribute باید یک نام دامنه معتبر باشد.',
+    'jwt' => 'مقدار :attribute باید یک توکن JWT معتبر باشد.',
+    'imei' => 'مقدار :attribute باید یک شماره IMEI معتبر باشد.',
+    'macaddress' => 'مقدار :attribute باید یک آدرس MAC معتبر باشد.',
+    'slug' => 'مقدار :attribute باید یک نام کوتاه موتور جستجو باشد.',
+    'semver' => 'مقدار :attribute باید یک شماره نسخه معتبر باشد.',
+    'luhn' => 'مقدار :attribute باید یک شماره Luhn معتبر باشد.',
+    'base64' => 'مقدار :attribute باید یک مقدار Base64 معتبر باشد.',
+    'issn' => 'مقدار :attribute باید یک شماره ISSN معتبر باشد.',
+    'lowercase' => 'مقدار :attribute باید فقط شامل حروف کوچک باشد.',
+    'uppercase' => 'مقدار :attribute باید فقط شامل حروف بزرگ باشد.',
+    'titlecase' => 'همه کلمات :attribute باید با حروف بزرگ آغاز شوند.',
+    'snakecase' => 'مقدار :attribute باید به صورت Snake Case باشد.',
+    'kebabcase' => 'مقدار :attribute باید به صورت Kebab Case باشد.',
+    'camelcase' => 'مقدار :attribute باید به صورت Camel Case باشد.',
+    'cidr' => 'مقدار :attribute باید یک نمایش CIDR معتبر باشد.',
+    'datauri' => 'مقدار :attribute باید یک URL داده معتبر باشد.',
+    'ulid' => 'مقدار :attribute باید یک ULID معتبر باشد.',
+    'ean' => 'مقدار :attribute باید یک EAN معتبر باشد.',
+    'gtin' => 'مقدار :attribute باید یک GTIN معتبر باشد.',
+    'postalcode' => 'مقدار :attribute باید یک کد پستی معتبر باشد.',
+    'mimetype' => 'مقدار :attribute باید یک نوع رسانه اینترنتی معتبر باشد.',
+];
